8762466d9d7969b2eb4df3dba60af55fb2ad8ec5
Kaushal Yadav <96814260+Kaushalyadav04@users.noreply.github.com>
2022-03-19 17:10:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var randomNumber1=Math.floor(Math.random()*6+1);
 
 
-var randomImage1="images/dice"+randomNumber1+".png";
+var randomImage1="dice"+randomNumber1+".png";
 
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImage1);
@@ -12,7 +12,7 @@ image1.setAttribute("src",randomImage1);
 // img 2
 
 var randomNumber2=Math.floor(Math.random()*6+1);
-var randomImage2="images/dice"+randomNumber2+".png";
+var randomImage2="dice"+randomNumber2+".png";
 
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomImage2) ;
